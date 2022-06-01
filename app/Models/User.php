bd0c15321741f9fb8a61a73031fae35a38101a09
Oleg Ilnytskyy <ilnytskyy67922@gmail.com>
2022-06-01 17:22:12 +0300

    2.5

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,12 +4,13 @@ declare(strict_types=1);
 
 namespace App\Models;
 
+use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 
-class User extends Authenticatable
+class User extends Authenticatable implements MustVerifyEmail
 {
     use HasApiTokens;
     use HasFactory;
@@ -24,6 +25,7 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'username',
     ];
 
     /**
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,16 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes(['verify' => true]);
+
+Route::group(['middleware' => ['auth','verified']], function(){
+    Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+    Route::group(['namespace' => 'Community','prefix' => 'communities' ],static function(){
+        Route::get('/', IndexController::class)->name('communities.index');
+        Route::get('/create', CreateController::class)->name('communities.create');
+    });
+});
+
+
